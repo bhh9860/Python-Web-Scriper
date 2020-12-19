4d7badc5fdee52347b0ec8333d314601e2b9ed15
Jeong Hyeon Tang <34750753+bhh9860@users.noreply.github.com>
2020-12-19 23:42:46 +0900

    무슨말인지 잘 모르겠다 처음이라 좀 어렵다 일단 2.5까지 완료

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_result = requests.get('http://www.indeed.com/jobs?q=python&limit=50')
+last_indeed_page = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
-
-print(indeed_soup)
+extract_indeed_jobs(last_indeed_page)
