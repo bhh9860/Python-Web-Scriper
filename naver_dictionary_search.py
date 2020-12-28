34b96103a5dbaeb075e35e5600af26f29679c1e6
Jeong Hyeon Tang <34750753+bhh9860@users.noreply.github.com>
2020-12-28 23:03:12 +0900

    음.. 강의를 한번 찾아봐야 할 듯 싶다.

--- naver_dictionary_search.py
+++ naver_dictionary_search.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib import parse
-urlencoding = '가나다'
+urlencoding = '단어'
 
 urlencodings = parse.quote(urlencoding)
 
@@ -16,4 +16,7 @@ html_result = html.find('div', {'id' : 'content'})
 
 korean = html_result.find('div', {'class' : 'kr_dic_section'})
 
-korean_soup = korean.find_all('ul',{'class' : 'lst_krdic'}).li
+a = str(korean.find_all('ul',{'class' : 'lst_krdic'})).replace('\t', '').replace('\r', '').replace('\n', '').split('</li>')
+
+for i in range(len(a)-1):
+  a[i].
